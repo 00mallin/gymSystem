e2fb359b002cca88212bc9beefb3115d0583bc5a
Malte Lindh <maltelindh@gmail.com>
2023-01-11 03:11:54 +0100

    Added NewMember function

--- UI/MembersUI.cs
+++ UI/MembersUI.cs
@@ -1,3 +1,6 @@
+using Logic;
+using Database;
+
 namespace UI;
 
 public class MembersUI
@@ -6,7 +9,7 @@ public class MembersUI
     {
         Menu subMenu = new();
 
-        subMenu.AddMenuItem("New Member", null);
+        subMenu.AddMenuItem("New Member", () => NewMember());
         subMenu.AddMenuItem("Update Membership", null);
         subMenu.AddMenuItem("Delete Member", null);
 
@@ -15,11 +18,24 @@ public class MembersUI
 
     private static void NewMember()
     {
+        Member newMember = new();
+
         Console.Clear();
         Console.Write("Personal number: ");
+        newMember.PersonalNumber = Console.ReadLine();
         Console.Write("First name: ");
+        newMember.FirstName = Console.ReadLine();
         Console.Write("Last name: ");
+        newMember.LastName = Console.ReadLine();
         Console.Write("Address: ");
+        newMember.Address = Console.ReadLine();
         Console.Write("Phone nr: ");
+        newMember.Phone = Console.ReadLine();
+
+        MemberDB.Add(newMember);
+        // Add member. if success
+        // Add new card
+        // Update memeber with card id
+        
     }
 }
