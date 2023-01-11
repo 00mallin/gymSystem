3f2073b3fb1d16aa6eb0dcfa655a0ada086b2175
Malte Lindh <maltelindh@gmail.com>
2023-01-11 06:29:57 +0100

    Changed Add Membership to AddOrEdit

--- Database/MembershipDB.cs
+++ Database/MembershipDB.cs
@@ -7,16 +7,27 @@ public class MembershipDB
 {
     private static ConnectDB db = new();
 
-    public static List<MembershipType> GetAll()
+    public static List<MembershipType> GetAllTypes()
     {
         return db.Connection.Query<MembershipType>("SELECT id, name, days, price_per_month AS PricePerMonth FROM membership_type").ToList();
     }
 
 
-    public static bool Add(Member member, MembershipType membershipType, int gymId)
+    public static bool AddOrEdit(Member member, MembershipType membershipType, int gymId)
     {
         Card card = CardDB.Get(member);
-        
+        List<Membership> activeMemberships = MembershipDB.GetCurrent(member);
+
+        // Remove already existing membership from DB 
+        // if there is already an existing membership at current gym
+        foreach (var membership in activeMemberships)
+        {
+            if(membership != null && membership.GymId == gymId)
+            {
+                Remove(membership.Id);
+            }
+        }
+
         int id = db.Connection.QuerySingleOrDefault<int>($"INSERT INTO membership(start_date, end_date, membership_type_id, gym_id) VALUES ('{DateTime.Now.ToString("yyyy-MM-dd")}', '{DateTime.Now.AddDays(membershipType.Days).ToString("yyyy-MM-dd")}', {membershipType.Id}, {gymId}); SELECT LAST_INSERT_ID()");
 
         if (id > 0)
@@ -25,6 +36,22 @@ public class MembershipDB
             return true;
         }
         
+        return false;
+    }
+
+    public static List<Membership> GetCurrent(Member member)
+    {
+        return db.Connection.Query<Membership>($"SELECT membership.id, membership.start_date AS StartDate, membership.end_date AS EndDate, membership.membership_type_id AS MembershipTypeId, membership.gym_id AS GymId FROM member INNER JOIN membership_card ON member.card_id = membership_card.card_id INNER JOIN membership ON membership_card.membership_id = membership.id WHERE member.id = {member.Id}").ToList();
+    }
+
+    public static bool Remove(int id)
+    {
+        var result = db.Connection.Execute($"DELETE FROM membership WHERE id = {id}");
+
+        if(result > 0)
+        {
+            return true;
+        }
         return false;
     }
 }
