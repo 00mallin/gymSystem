2f002290c1f77d314233ad19238bc84e78417234
Malte Lindh <maltelindh@gmail.com>
2023-01-11 05:06:57 +0100

    Added EditMembership UI

--- UI/MembersSubUI.cs
+++ UI/MembersSubUI.cs
@@ -5,15 +5,37 @@ namespace UI;
 
 public class MembersSubUI
 {
-    public static void Show(int gymId)
+    public static void Show(int gymId, Member member)
     {
+        Console.WriteLine(member.FirstName);
         Menu subMenu = new();
 
-        subMenu.AddMenuItem("Edit Membership", () => EditMembership(gymId));
-        subMenu.AddMenuItem("Cancel Membership", () => CancelMembership());
-        subMenu.AddMenuItem("Remove Member", () => RemoveMember());
+        subMenu.AddMenuItem("Edit Membership", () => EditMembership(gymId, member));
+        // subMenu.AddMenuItem("Cancel Membership", () => CancelMembership());
+        // subMenu.AddMenuItem("Remove Member", () => RemoveMember());
 
 
         subMenu.Show();
+    }
+
+    private static void EditMembership(int gymId, Member member)
+    {
+        Console.Clear();
+        List<MembershipType> memberships = MembershipDB.GetAll();
+
+        for (int i = 0; i < memberships.Count; i++)
+        {
+            Console.WriteLine($"{i+1}. {memberships[i].Name} ({memberships[i].PricePerMonth} SEK)");
+        }
+        Console.Write("Select membership: ");
+        int selection = int.Parse(Console.ReadLine());
+
+        MembershipType membershipType = memberships[selection-1];
+
+        if(MembershipDB.Add(member, membershipType, gymId))
+        {
+            Console.Write("Membership succesfully started!...");
+            Console.ReadKey();
+        }
     }
 }
