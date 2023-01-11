3225074c2cb3866b3183ad69433a382a6e502a45
Malte Lindh <maltelindh@gmail.com>
2023-01-11 04:28:42 +0100

    Added Days property

--- Logic/MembershipType.cs
+++ Logic/MembershipType.cs
@@ -4,6 +4,7 @@ public class MembershipType
 {
     public int Id { get; set; }
     public string Name { get; set; }
+    public int Days { get; set; }
     public float PricePerMonth { get; set; }
 
     public MembershipType() { }
